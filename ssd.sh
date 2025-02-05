0fd9fcaf21e4a18be72ebd2813f7bca3d4029c29
Eccys <73040912+Eccys@users.noreply.github.com>
2025-02-05 01:38:57 +0000

    Use non-compressable data (/dev/urandom)

--- ssd.sh
+++ ssd.sh
@@ -3,27 +3,29 @@
 OUTPUT_FILE="/opt/tmp"
 
 add_cron_job() {
-  # Ensure the cron job is added only once
-  (crontab -l 2>/dev/null; echo "* * * * * /bin/bash $(realpath $0) -auto") | crontab -
-  if [[ $? -eq 0 ]]; then
-    echo "Cron job added to run this script every minute."
-  else
-    echo "Failed to add cron job."
-  fi
+    (
+        crontab -l 2>/dev/null
+        echo "* * * * * /bin/bash $(realpath $0) -auto"
+    ) | crontab -
+    if [[ $? -eq 0 ]]; then
+        echo "Cron job added to run this script every minute."
+    else
+        echo "Failed to add cron job."
+    fi
 }
 
 if [[ "$1" == "-auto" ]]; then
-  add_cron_job
-  exit 0
+    add_cron_job
+    exit 0
 fi
 
 start_time=$(date +%s)
 
 while [ $(($(date +%s) - $start_time)) -lt 59 ]; do
-  for i in {1..5}; do
-    dd if=/dev/zero of="${OUTPUT_FILE}_$i" bs=1G count=1 oflag=direct status=none &
-  done
-  wait
+    for i in {1..5}; do
+        dd if=/dev/urandom of="${OUTPUT_FILE}_$i" bs=1M count=1024 oflag=direct status=none &
+    done
+    wait
 done
 
 echo "Data write complete for this minute."
